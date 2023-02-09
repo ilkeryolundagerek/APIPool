598d33dd6f15082a2be81485b4919677815946da
BT06 <BT06@BT06>
2023-02-09 15:53:39 +0300

    09.02.2023 16:00

---
+++ APIGateway/Program.cs
@@ -0,0 +1,24 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services
+    .AddOcelot(new ConfigurationBuilder().AddJsonFile("ocelot.json").Build())
+    .AddCacheManager(o => o.WithDictionaryHandle());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseOcelot();
+app.Run();
